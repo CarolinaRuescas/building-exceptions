bc37c756b9194f916c776401278901edbf0678f0
Carol <carol@b25>
2025-04-10 11:54:15 +0200

    añadiendo test

---
+++ src/test/java/org/ies/building/components/BuildingTests.java
@@ -0,0 +1,20 @@
+package org.ies.building.components;
+import org.ies.building.model.Apartment;
+import org.ies.building.model.Building;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BuildingTests {
+    @Test
+    public void findApartmentTest() throws EmptyStackException{
+        // preparación del test
+
+        Building building = new Building();
+        List<Apartment>
+    }
+}
